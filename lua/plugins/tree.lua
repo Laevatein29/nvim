4bd1a13718073090dd2ce0ce6ba1d963586f2162
laevatein29 <liyongchen29@gmail.com>
2024-07-15 10:46:27 +0800

    feat: add snippets

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -5,5 +5,22 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  config = function() require("nvim-tree").setup {} end,
+  config = function()
+    require("nvim-tree").setup {
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
+        },
+      },
+    }
+  end,
 }
